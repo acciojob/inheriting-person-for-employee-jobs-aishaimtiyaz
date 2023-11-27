b9cd952a66667393b39fb148a5ed88d4e7505bc7
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-27 22:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-// complete this js code
-function Person(name, age) 
-{
-	this.name = name;
-	this.age = age;
+// Person constructor function
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
-function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle = jobTitle;
-}
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-Employee.prototype.jobGreet = function(){
-console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// Adding greet method to the Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
+
+// Employee constructor function that inherits from Person
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor using the Employee instance
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inheriting the Person prototype in the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// Setting the constructor of Employee back to Employee
+Employee.prototype.constructor = Employee;
+
+// Adding jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
