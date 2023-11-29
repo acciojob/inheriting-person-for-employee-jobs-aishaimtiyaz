7c44ee4f08ec8026f7733fa0dd6aad6d1f1868aa
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-29 22:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,28 +4,31 @@ function Person(name, age) {
   this.age = age;
 }
 
-// Adding greet method to the Person prototype
+// Adding a greet method to the Person prototype
 Person.prototype.greet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
 // Employee constructor function that inherits from Person
 function Employee(name, age, jobTitle) {
-  // Call the Person constructor using the Employee instance
+  // Call the Person constructor using Employee's context
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inheriting the Person prototype in the Employee prototype
+// Inherit the Person prototype in the Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 
-// Setting the constructor of Employee back to Employee
+// Fix the constructor property to point back to Employee
 Employee.prototype.constructor = Employee;
 
-// Adding jobGreet method to the Employee prototype
+// Adding a jobGreet method to the Employee prototype
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
+
+
+
 
 
 
